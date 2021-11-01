2c6a70f78de1a576834b1ad4a5c8940c5ed2258b
Thatchapatt (Tar) <74225067+utpk@users.noreply.github.com>
2021-11-01 12:43:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 import http.server
 import socketserver
 from http import HTTPStatus
-import camelcase
-
-c = camelcase.CamelCase()
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
-        self.wfile.write(c.hump('hello world').encode())
+        self.wfile.write('hello world')
 
 httpd = socketserver.TCPServer(('', 8080), Handler)
 httpd.serve_forever()
