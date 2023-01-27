6374abd6dcb7587b65fedeebf33c0031fcb7cec3
Thatchapatt (Tar) <74225067+utpk@users.noreply.github.com>
2023-01-27 14:08:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
-import http.server
-import socketserver
-from http import HTTPStatus
-import camelcase
+from fastapi import FastAPI
 
-c = camelcase.CamelCase()
+app = FastAPI()
 
-class Handler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(HTTPStatus.OK)
-        self.end_headers()
-        self.wfile.write(c.hump('hello world').encode())
 
-httpd = socketserver.TCPServer(('', 8080), Handler)
-httpd.serve_forever()
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/201", status_code=201)
+async def r201():
+    return {"message": "created"}
+
+@app.get("/404", status_code=404)
+async def r404():
+    return {"message": "not found"}
+
+@app.get("/500", status_code=500)
+async def r500():
+    return {"message": "something went wrong"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "app:app",
+        host="0.0.0.0",
+        port=8080,
+        reload=True,
+        proxy_headers=True,
+        server_header=False,
+        forwarded_allow_ips="*",
+    )
